abf4dbdcace091f78abe7e076d5bd3d74916f9f8
Mauricio Mathias <mauricioneis15@gmail.com>
2025-01-24 19:27:07 +0300

    Subindo Busca de usuário por ID

--- cypress/e2e/backend/usuarios.cy.js
+++ cypress/e2e/backend/usuarios.cy.js
@@ -32,8 +32,24 @@ describe('Cenário dos usuários', () => {
       //Verifica o retorno do serviço
       expect(cadastroUsuariosResponse.status).to.equal(201)
       expect(cadastroUsuariosResponse.body.message).to.have.string('Cadastro realizado com sucesso')
-      expect(cadastroUsuariosResponse.body._id).to.exist
       expect(cadastroUsuariosResponse.body._id).to.be.a('string')
+    })
+  })
+
+  it('Caminho feliz - Busca de usuário por ID', () => {
+    //Realiza a chamada do serviço de criação dos usuários
+    cy.request({
+      method: 'GET',
+      url: backUrl+'/usuarios/'+idUsuario
+    }).then((buscaUsuariosResponse) => {
+      
+      //Verifica o retorno do serviço
+      expect(buscaUsuariosResponse.status).to.equal(200)
+      expect(buscaUsuariosResponse.body.nome).to.have.string(nome)
+      expect(buscaUsuariosResponse.body.email).to.have.string(email)
+      expect(buscaUsuariosResponse.body.password).to.have.string(password)
+      expect(buscaUsuariosResponse.body.administrador).to.have.string(admin)
+      expect(buscaUsuariosResponse.body._id).to.exist
     })
   })
 })
