855609ad1ebd6c22fcd9d1b63aa0bc21767b420b
Mauricio Mathias <mauricioneis15@gmail.com>
2025-01-25 12:56:15 +0300

    alterando o cenario para produto duplicado

---
+++ cypress/e2e/backend/carrinhos.cy.js
@@ -0,0 +1,55 @@
+/// <reference types="cypress" />
+
+const generator = require('random-password'); //Esse pacote é um gerador de senha aleatório
+
+describe('Cenários dos produtos', () => {
+
+  //Classes e variáveis
+  const backUrl = Cypress.env('backUrl')
+  var token
+
+  //Decidi deixar o nome e descrição dos carrinhos como um gerador de senha aleatório
+  //Isso porque o gerador de senha gera vários caracteres aleatórios
+  //Fica bom para simular qualquer caractere sendo adicionado pelo sistema ao carrinho
+
+  //Variaveis de criação do carrinho
+  var idProduto = generator(10) //id do produto
+  var idSegundoProduto  = generator(10) //id do segundo produto
+
+  it('Login para buscar o token', () => {
+    //Realiza o login pra buscar o token 
+    cy.login().then((loginResponse) => {
+
+      token = loginResponse
+    })
+  })
+
+  it('Fluxo de exceção - Cadastro de carrinho, produto duplicado', () => {
+      
+    //Realiza a chamada do serviço de cadastro dos carrinhos
+    cy.request({
+    method: 'POST',
+    url: backUrl+'/carrinhos',
+    headers: {
+        Authorization: `Bearer${token}`,
+    },
+    body: {
+        produtos: [
+          {
+            idProduto: idProduto,
+            quantidade: 100
+          },
+          {
+            idProduto: idProduto,
+            quantidade: 200
+          }
+        ]
+      },
+    failOnStatusCode: false
+    }).then((cadastroProdutosResponse) => {
+
+        expect(cadastroProdutosResponse.status).to.equal(400)
+        expect(cadastroProdutosResponse.body.message).to.have.string('Não é permitido possuir produto duplicado')
+    })
+  })
+})
